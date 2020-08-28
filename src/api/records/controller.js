893d20a0b35b6b67ac456128814401bb94447663
ikipyegon <ikipyegon@safaricom.co.ke>
2020-08-29 00:35:23 +0300

    feat(): add controller method to handle records search

---
+++ src/api/records/controller.js
@@ -0,0 +1,57 @@
+import { success, notFound } from '../../services/response/'
+import { Records } from '.'
+
+export const create = ({ bodymen: { body } }, res, next) =>
+  Records.create(body)
+    .then((records) => records.view(true))
+    .then(success(res, 201))
+    .catch(next)
+
+export const search = ({ bodymen: { body: { startDate, endDate, minCount, maxCount  } } }, res, next) =>
+  Records.aggregate([
+    {
+      // Filter by dates.
+      $match: {
+        createdAt: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate)
+        }
+      }
+    },
+    {
+      // Flatten counts arrays
+      $unwind: {
+        path: '$counts'
+      }
+    },
+    {
+      // Group by key and createdAt
+      $group: {
+        _id: { key: '$key', createdAt: '$createdAt' },
+        key: { $first: '$key' },
+        createdAt: { $first: '$createdAt' },
+        totalCount: {
+          $sum: '$counts'
+        }
+      }
+    },
+    {
+      // Filter by minCount and maxCount
+      $match: {
+        totalCount: {
+          $gte: minCount,
+          $lte: maxCount
+        }
+      }
+    },
+    {$sort: {"createdAt": -1} },
+    {
+      // Exclude _id in resultset
+      $project: {
+        _id: 0
+      }
+    }
+  ]).exec()
+    .then((records) => records)
+    .then(success(res))
+    .catch(next)
