a5aef2ccca29b32b54b1c9cded51bfd9c4af8c2a
ikipyegon <ikipyegon@safaricom.co.ke>
2020-08-29 00:34:42 +0300

    feat(): add records model

---
+++ src/api/records/model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from 'mongoose'
+
+const recordsSchema = new Schema({
+  key: {
+    type: String
+  },
+  value: {
+    type: String
+  },
+  counts: [{
+    type: Number
+  }],
+  createdAt: {
+    type: Date
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (obj, ret) => { delete ret._id }
+  }
+})
+
+recordsSchema.methods = {
+  view (full) {
+    const view = {
+      // simple view
+      id: this.id,
+      key: this.key,
+      value: this.value,
+      counts: this.counts,
+      createdAt: this.createdAt,
+    }
+
+    return full ? {
+      ...view
+      // add properties for a full view
+    } : view
+  }
+}
+
+const model = mongoose.model('Records', recordsSchema)
+
+export const schema = model.schema
+export default model
