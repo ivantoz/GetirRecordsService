7f11fbfc03e6d3b4201d5ee0293a97f20273848a
ikipyegon <ikipyegon@safaricom.co.ke>
2020-08-29 00:36:03 +0300

    chore(): add tests for records search endpoint

---
+++ src/api/records/index.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { apiRoot } from '../../config'
+import express from '../../services/express'
+import routes, { Records } from '.'
+
+const app = () => express(apiRoot, routes)
+
+let records
+
+beforeEach(async () => {
+  records = await Records.create({})
+})
+
+test('POST /records 200', async () => {
+  const { status, body } = await request(app())
+    .post(`${apiRoot}`)
+    .send({
+      "startDate": "2016-01-26",
+      "endDate": "2018-02-20",
+      "minCount": 2700,
+      "maxCount": 3000
+    })
+  expect(status).toBe(200)
+  expect(typeof body).toEqual('object')
+  expect(body.key).toEqual('test')
+  expect(body.value).toEqual('test')
+  expect(body.counts).toEqual('test')
+  expect(body.createdAt).toEqual('test')
+})
+
+
+
