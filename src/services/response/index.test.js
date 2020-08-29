25575f04f8023e0b0ffae9de80efdeba5ef16170
ikipyegon <ikipyegon@safaricom.co.ke>
2020-08-29 16:02:07 +0300

    refactor(): change test cases for success response service

--- src/services/response/index.test.js
+++ src/services/response/index.test.js
@@ -12,33 +12,14 @@ beforeEach(() => {
 
 describe('success', () => {
   it('responds with passed object and status 200', () => {
-    expect(response.success(res)({ prop: 'value' })).toBeNull()
+    expect(response.success(res)(['record_object'])).toBeNull()
     expect(res.status).toBeCalledWith(200)
-    expect(res.json).toBeCalledWith({ prop: 'value' })
+    expect(res.json).toBeCalledWith({"code": 0, "msg": "Success", "records": ["record_object"]})
   })
 
-  it('responds with passed object and status 201', () => {
-    expect(response.success(res, 201)({ prop: 'value' })).toBeNull()
-    expect(res.status).toBeCalledWith(201)
-    expect(res.json).toBeCalledWith({ prop: 'value' })
-  })
-
-  it('does not send any response when object has not been passed', () => {
-    expect(response.success(res, 201)()).toBeNull()
-    expect(res.status).not.toBeCalled()
-  })
-})
-
-describe('notFound', () => {
-  it('responds with status 404 when object has not been passed', () => {
-    expect(response.notFound(res)()).toBeNull()
-    expect(res.status).toBeCalledWith(404)
-    expect(res.end).toHaveBeenCalledTimes(1)
-  })
-
-  it('returns the passed object and does not send any response', () => {
-    expect(response.notFound(res)({ prop: 'value' })).toEqual({ prop: 'value' })
-    expect(res.status).not.toBeCalled()
-    expect(res.end).not.toBeCalled()
+  it('responds with passed object and status 400', () => {
+    expect(response.success(res)([])).toBeNull()
+    expect(res.status).toBeCalledWith(400)
+    expect(res.json).toBeCalledWith({ code: 400, msg: 'Bad request' })
   })
 })
